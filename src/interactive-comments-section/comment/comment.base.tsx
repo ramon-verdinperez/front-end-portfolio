8098a572d4dc80d02b45e25ccb0c7171607b38b7
Ramon Verdin <ramon.verdinperez@hcl.com>
2024-01-03 17:23:44 -0800

    Adding reply

--- src/interactive-comments-section/comment/comment.base.tsx
+++ src/interactive-comments-section/comment/comment.base.tsx
@@ -2,27 +2,59 @@ import style from "./comment.module.css";
 import plusIcon from "../images/icon-plus.svg";
 import minusIcon from "../images/icon-minus.svg";
 import replyIcon from "../images/icon-reply.svg";
-import { CommentType } from "../comment.types";
+import { CommentType, User } from "../comment.types";
+import { useState } from "react";
 
 export interface CommentProps {
   comment: CommentType;
+  currentUser: User;
 }
 
 export const Comment = (props: CommentProps) => {
-  const { comment } = props;
+  const { comment, currentUser } = props;
+  const [renderReply, setRenderReply] = useState(false);
+
+  const replyToComment = () => {
+    const newReply: CommentType = {
+      id: 1234,
+      content: "",
+      createdAt: "Today",
+      repliesTo: comment.user.username,
+      score: 0,
+      user: currentUser,
+    };
+
+    return (
+      <div className={`${style.newComment} ${style.commentBase}`}>
+        <img
+          className={style.avatar}
+          src={currentUser.image.png}
+          alt="Current User Avatar"
+        />
+        <textarea
+          rows={4}
+          content={newReply.repliesTo}
+          className={style.newCommentInput}
+        />
+        <button className={style.newCommentReply}>
+          <p>REPLY</p>
+        </button>
+      </div>
+    );
+  };
 
   return (
     <div>
-      <div className={style.outer}>
+      <div className={`${style.regularComment} ${style.commentBase}`}>
         <div className={style.likeCountOuter}>
           <div className={style.likeCounter}>
-            <div className={style.likeCounterItem}>
+            <div className={`${style.likeCounterItem} ${style.button}`}>
               <img src={plusIcon} alt="Plus Button" />
             </div>
             <div className={style.likeCounterItem}>
               <p>{comment.score}</p>
             </div>
-            <div className={style.likeCounterItem}>
+            <div className={`${style.likeCounterItem} ${style.button}`}>
               <img src={minusIcon} alt="Minus Button" />
             </div>
           </div>
@@ -36,7 +68,10 @@ export const Comment = (props: CommentProps) => {
             />
             <h4>{comment.user.username}</h4>
             <p>{comment.createdAt}</p>
-            <div className={style.replyButton}>
+            <div
+              className={`${style.replyButton} ${style.button}`}
+              onClick={() => setRenderReply(!renderReply)}
+            >
               <img src={replyIcon} alt="Reply" />
               <p>Reply</p>
             </div>
@@ -46,12 +81,13 @@ export const Comment = (props: CommentProps) => {
           </div>
         </div>
       </div>
+      {renderReply && replyToComment()}
       {comment.replies && comment.replies?.length > 0 && (
         <div className={style.replySection}>
           <div className={style.verticalLine}></div>
           <div className={style.reply}>
             {comment.replies.map((r: CommentType) => (
-              <Comment comment={r} />
+              <Comment comment={r} currentUser={currentUser} />
             ))}
           </div>
         </div>
--- src/interactive-comments-section/socialFeed/socialFeedMain.base.tsx
+++ src/interactive-comments-section/socialFeed/socialFeedMain.base.tsx
@@ -1,16 +1,17 @@
-import { CommentType } from "../comment.types";
+import { CommentType, User } from "../comment.types";
 import { Comment } from "../comment/comment.base";
 import style from "./socialFeedMain.module.css";
 import data from "../data.json";
 
 export const SocialFeedMain = () => {
   const comments: CommentType[] = data.comments;
+  const currentUser: User = data.currentUser;
 
   return (
     <div className={style.outer}>
       <div className={style.socialFeed}>
-        {comments.map((c) => (
-          <Comment comment={c as unknown as CommentType} />
+        {comments.map((c: CommentType) => (
+          <Comment comment={c} currentUser={currentUser}/>
         ))}
       </div>
     </div>
