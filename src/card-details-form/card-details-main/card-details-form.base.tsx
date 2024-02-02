388b5869b47eb9e15f5a2d64ae4a613ff7727a5b
Ramon Verdin <ramon.verdinperez@hcl.com>
2024-02-01 16:22:29 -0800

    Styling completed

--- src/card-details-form/card-details-main/card-details-form.base.tsx
+++ src/card-details-form/card-details-main/card-details-form.base.tsx
@@ -11,37 +11,33 @@ export const CardDetailsForm = () => {
       <div className={styles.formContentOuter}>
         <form className={styles.cardForm}>
           <div className={styles.genericInput}>
-            <label>CARDHOLDER NAME</label>
+            <h4>CARDHOLDER NAME</h4>
             <input type="text" placeholder="e.g. Jane Appleseed"></input>
           </div>
 
           <div className={styles.genericInput}>
-            <label> CARD NUMBER</label>
+            <h4>CARD NUMBER</h4>
             <input type="text" placeholder="e.g. 1234 5678 9123 0000"></input>
           </div>
 
           <div className={styles.dateAndCVC}>
             <div style={{ display: "flex", flexDirection: "column" }}>
-              <label>EXP. DATE (MM/YY)</label>
-                          <div style={{ display: "flex", flexDirection: "row", gap: '5px' }}>
-                <input
-                  type="text"
-                  placeholder="MM"
-                ></input>
-                <input
-                  type="text"
-                  placeholder="YY"
-                ></input>
+              <h4>EXP. DATE (MM/YY)</h4>
+              <div
+                style={{ display: "flex", flexDirection: "row", gap: "5px" }}
+              >
+                <input type="text" placeholder="MM"></input>
+                <input type="text" placeholder="YY"></input>
               </div>
             </div>
 
             <div style={{ display: "flex", flexDirection: "column" }}>
-              <label>CVC</label>
-              <input
-                type="text"
-                placeholder="e.g. 123"
-              ></input>
+              <h4>CVC</h4>
+              <input type="text" placeholder="e.g. 123"></input>
             </div>
+          </div>
+          <div className={styles.confirmButton}>
+            <h4>Confirm</h4>
           </div>
         </form>
       </div>
