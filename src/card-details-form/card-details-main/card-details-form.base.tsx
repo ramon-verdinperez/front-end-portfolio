66a5029f218924f236bccd9b4c4549c8526879a7
Ramon Verdin <ramonverdin17@gmail.com>
2024-02-07 16:47:50 -0800

    Completed form functionality

--- src/card-details-form/card-details-main/card-details-form.base.tsx
+++ src/card-details-form/card-details-main/card-details-form.base.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import styles from "./card-details-form.module.css";
 import { useForm } from "react-hook-form";
+import checkSuccess from "../assets/icon-complete.svg";
 
 export const CardDetailsForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    reset,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -14,21 +18,48 @@ export const CardDetailsForm = () => {
       expYear: undefined,
       cvc: undefined,
     },
-    delayError: 100,
+    delayError: 0,
     mode: "onBlur",
   });
 
-  return (
-    <div className={styles.outer}>
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+
+  const renderCards = () => {
+    return (
       <div className={styles.cardsContainer}>
-        <div className={styles.frontCard}></div>
-        <div className={styles.backCard}></div>
+        <div className={styles.frontCard}>
+          <div>{""}</div>
+          <div>
+            <p className={styles.cardNumberOnCard}>
+              {watch("cardNumber")
+                .match(/.{1,4}/g)
+                ?.join(" ") || "0000 0000 0000 0000"}
+            </p>
+          </div>
+          <div className={styles.cardNameAndExp}>
+            <p>{watch("name") || "Jane Appleseed"}</p>
+            <span className={styles.cardExp}>
+              <p>
+                {watch("expMonth") || "00"}/{watch("expYear") || "00"}
+              </p>
+            </span>
+          </div>
+        </div>
+        <div className={styles.backCard}>
+          <span className={styles.cardCVConCard}>
+            <p>{watch("cvc") || "000"}</p>
+          </span>
+        </div>
       </div>
-      <div className={styles.leftImage} />
-      <div className={styles.formContentOuter}>
+    );
+  };
+
+  const renderForm = () => {
+    return (
+      <>
         <form
           className={styles.cardForm}
-          onSubmit={handleSubmit((d) => console.log(d))}
+          onSubmit={handleSubmit(() => setSubmitSuccess(true))}
         >
           <div className={styles.genericInput}>
             <h4>CARDHOLDER NAME</h4>
@@ -60,7 +91,9 @@ export const CardDetailsForm = () => {
                 },
               })}
               maxLength={16}
-              className={errors.cardNumber?.message ? styles.inputError : undefined}
+              className={
+                errors.cardNumber?.message ? styles.inputError : undefined
+              }
             />
             {errors.cardNumber?.message && (
               <div>
@@ -84,11 +117,13 @@ export const CardDetailsForm = () => {
                     required: "Can't be blank",
                     pattern: {
                       value: /^0[0-9]|1[1-2]$/,
-                      message: "2 digit month",
+                      message: "2 digit month (1-12)",
                     },
                   })}
                   maxLength={2}
-                  className={errors.expMonth?.message ? styles.inputError : undefined}
+                  className={
+                    errors.expMonth?.message ? styles.inputError : undefined
+                  }
                 />
                 <input
                   type="text"
@@ -97,11 +132,13 @@ export const CardDetailsForm = () => {
                     required: "Can't be blank",
                     pattern: {
                       value: /^[0-9]{2}$/,
-                      message: "2 digit month",
+                      message: "2 digit year",
                     },
                   })}
                   maxLength={2}
-                  className={errors.expYear?.message ? styles.inputError : undefined}
+                  className={
+                    errors.expYear?.message ? styles.inputError : undefined
+                  }
                 />
               </div>
               {(errors.expMonth?.message || errors.expYear?.message) && (
@@ -129,7 +166,6 @@ export const CardDetailsForm = () => {
                   },
                 })}
                 maxLength={3}
-
                 className={errors.cvc?.message ? styles.inputError : undefined}
               />
               {errors.cvc?.message && (
@@ -145,6 +181,36 @@ export const CardDetailsForm = () => {
             value="Confirm"
           />
         </form>
+      </>
+    );
+  };
+
+  const renderSubmitSuccess = () => {
+    return (
+      <div className={styles.submitSuccessOuter}>
+        <img src={checkSuccess} alt="Successful Submission Checkmark" />
+        <h1>THANK YOU!</h1>
+        <h4>We've added your card details</h4>
+        <input
+          style={{ marginTop: "40px" }}
+          onClick={() => {
+            reset();
+            setSubmitSuccess(false);
+          }}
+          className={styles.confirmButton}
+          type="submit"
+          value="Continue"
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.outer}>
+      {renderCards()}
+      <div className={styles.leftImage} />
+      <div className={styles.contentOuter}>
+        {submitSuccess ? renderSubmitSuccess() : renderForm()}
       </div>
     </div>
   );
