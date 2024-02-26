4e4055c3d8c83fb30af6a8b1caa3dfcbad56150c
Ramon Verdin <ramonverdin17@gmail.com>
2024-02-26 14:59:01 -0800

    Added to about me

--- src/about-me/aboutMe.base.tsx
+++ src/about-me/aboutMe.base.tsx
@@ -16,6 +16,8 @@ import tsIcon from "./assets/Technologies/typescript.png";
 import cppIcon from "./assets/Technologies/cpp.png";
 import pythonIcon from "./assets/Technologies/python.png";
 import apiIcon from "./assets/Technologies/api.png";
+import pwIcon from "./assets/Technologies/playwright.png";
+import jestIcon from "./assets/Technologies/jest.png";
 import styles from "./aboutMe.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -24,24 +26,91 @@ import { TypeWriterText } from "../type-writer/typeWriter.base";
 interface Technologies {
   text: string;
   link: string;
+  proficiency: number;
 }
 
 export const AboutMe = () => {
   const tech: Technologies[] = [
-    { text: "Javascript", link: jsIcon },
-    { text: "Typescript", link: tsIcon },
-    { text: "React", link: reactIcon },
-    { text: "Redux", link: reduxIcon },
-    { text: "APIs", link: apiIcon },
-    { text: "HTML", link: htmlIcon },
-    { text: "CSS", link: cssIcon },
-    { text: "Git", link: gitIcon },
-    { text: "Azure", link: azureIcon },
-    { text: "MySql", link: sqlIcon },
-    { text: "Firebase", link: firebaseIcon },
-    { text: "Postman", link: postmanIcon },
-    { text: "C++", link: cppIcon },
-    { text: "Python", link: pythonIcon },
+    {
+      text: "Javascript",
+      link: jsIcon,
+      proficiency: 8,
+    },
+    {
+      text: "Typescript",
+      link: tsIcon,
+      proficiency: 8,
+    },
+    {
+      text: "React",
+      link: reactIcon,
+      proficiency: 8,
+    },
+    {
+      text: "Redux",
+      link: reduxIcon,
+      proficiency: 6,
+    },
+    {
+      text: "Playwright",
+      link: pwIcon,
+      proficiency: 5,
+    },
+    {
+      text: "Jest",
+      link: jestIcon,
+      proficiency: 5,
+    },
+    {
+      text: "APIs",
+      link: apiIcon,
+      proficiency: 8,
+    },
+    {
+      text: "HTML",
+      link: htmlIcon,
+      proficiency: 8,
+    },
+    {
+      text: "CSS",
+      link: cssIcon,
+      proficiency: 8,
+    },
+    {
+      text: "Git",
+      link: gitIcon,
+      proficiency: 8,
+    },
+    {
+      text: "Azure",
+      link: azureIcon,
+      proficiency: 5,
+    },
+    {
+      text: "MySql",
+      link: sqlIcon,
+      proficiency: 6,
+    },
+    {
+      text: "Firebase",
+      link: firebaseIcon,
+      proficiency: 6,
+    },
+    {
+      text: "Postman",
+      link: postmanIcon,
+      proficiency: 6,
+    },
+    {
+      text: "C++",
+      link: cppIcon,
+      proficiency: 7,
+    },
+    {
+      text: "Python",
+      link: pythonIcon,
+      proficiency: 6,
+    },
   ];
 
   return (
@@ -110,16 +179,57 @@ export const AboutMe = () => {
           <FontAwesomeIcon icon={faChevronDown} size="2xl" />
         </div>
       </div>
-      <div className={styles.aboutMeContainer}></div>
-      <div className={styles.technologiesOuterContainer}>
-        {tech.map((t: Technologies) => {
-          return (
-            <div className={styles.techContainer} key={t.text}>
-              <img src={t.link} alt={t.text} />
-              <p>{t.text}</p>
-            </div>
-          );
-        })}
+      <div className={styles.aboutMeContainer}>
+        <div className={styles.aboutMeContent}>
+          <h1 className={styles.aboutMeTitle}>About Me</h1>
+          <p className={styles.aboutMeSummary}>
+            I am Ramon Verdin a San Francisco Bay Area native with a Bacehelor's
+            degree in Computer Science from the University of California, Santa
+            Barbara. I am in the early stages of my career in the search for
+            employment. In my first professional positin as a software developer
+            with HCLTech, I learned a lot and was able to get a glimpse at how
+            the professional world of a software developer. I learned many
+            technologies and tools and was able to put them to the test. Despite
+            my lack of professional experience, my determination and ability to
+            quickly learn has helped me and will help me in succeeding.
+            <br />
+            <br />
+          </p>
+
+          <h1 className={styles.aboutMeTitle}>Technologies</h1>
+          <p className={styles.aboutMeSummary}>
+            While I am no master in one particular technology, my ability to
+            quickly learn and adapt has led me to become proficient in several
+            technologies. My primary focus and strongest skill has to be
+            Frontend development. I was lucky enough to hone in on this skill in
+            my previous employment at Microsoft (through contract with HCLTech).
+            In this position I had the opportunity to work alongside several
+            Microsoft engineers, designers, and project stakeholders to develop
+            a web application using React, Typescript, and several other
+            technologies. While this was my first official application that made
+            it into production, it was a great way to learn and develop my
+            skills while also learning the tools of the trade. It taught me
+            invaluable lessons that will remain with me for my career to come.
+            <br />
+            <br />
+          </p>
+          <div className={styles.technologiesOuterContainer}>
+            {tech.map((t: Technologies) => {
+              return (
+                <div className={styles.techContainer} key={t.text}>
+                  <img src={t.link} alt={t.text} />
+                  <p>{t.text}</p>
+                  <div className={styles.progressOuter}>
+                    <div
+                      className={styles.progressInner}
+                      style={{ width: `${t.proficiency}0%` }}
+                    ></div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
